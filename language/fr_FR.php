1bc31a61376f1392e4e1c486b9bfad62590a9ccd
Thomas ROGER <thomaroger@gmail.com>
2013-10-16 00:43:23 +0200

    implements state for game, PLAYGROUND-172

--- language/fr_FR.php
+++ language/fr_FR.php
@@ -1,133 +1,138 @@
 <?php
 return array(
 
-        // Breadcrumbs
-        'Home' 				=> 'Accueil',
+    // Breadcrumbs
+    'Home' 				=> 'Accueil',
 
-        // Admin List
-        'Title' 			=> 'Titre',
-        'Created at' 		=> 'Création',
-        'Start Date' 		=> 'Début',
-        'End Date' 			=> 'Fin',
-        'Slug'  			=> 'Lien',
-        'Edit' 				=> 'Editer',
-        'Delete' 			=> 'Supprimer',
-        'Add options' 		=> 'Ajouter des options',
-        'Add new game' 		=> 'Ajouter un jeu',
-        'Add new lottery' 	=> 'Ajouter un tirage au sort',
-        'Add new Quiz' 		=> 'Ajouter un quizz',
-        'Add new Instant Win' => 'Ajouter un instant gagnant',
-        'Add new Post & Vote' => 'Ajouter un Post & Vote',
-        'Active' 			=> 'Actif',
-        'Activate the game'             => 'Activer le jeu',
-        'Desactivate the game'             => 'Desactiver le jeu',
-        'Yes' 				=> 'Oui',
-        'No' 				=> 'Non',
-        'Leader Board' 		=> 'Participants',
-        'Add new occurrence'=> 'Ajouter un instant gagnant',
+    // Admin List
+    'Title' 			=> 'Titre',
+    'Created at' 		=> 'Création',
+    'Start Date' 		=> 'Début',
+    'End Date' 			=> 'Fin',
+    'Slug'  			=> 'Lien',
+    'Edit' 				=> 'Editer',
+    'Delete' 			=> 'Supprimer',
+    'Add options' 		=> 'Ajouter des options',
+    'Add new game' 		=> 'Ajouter un jeu',
+    'Add new lottery' 	=> 'Ajouter un tirage au sort',
+    'Add new Quiz' 		=> 'Ajouter un quizz',
+    'Add new Instant Win' => 'Ajouter un instant gagnant',
+    'Add new Post & Vote' => 'Ajouter un Post & Vote',
+    'Active' 			=> 'Actif',
+    'Activate the game'             => 'Activer le jeu',
+    'Desactivate the game'             => 'Desactiver le jeu',
+    'Yes' 				=> 'Oui',
+    'No' 				=> 'Non',
+    'Leader Board' 		=> 'Participants',
+    'Add new occurrence'=> 'Ajouter un instant gagnant',
 
-        // Paginator
-        'First' 			=> 'Premier',
-        'Previous' 			=> 'Précédent',
-        'Next' 				=> 'Suivant',
-        'Last' 				=> 'Dernier',
+    // Paginator
+    'First' 			=> 'Premier',
+    'Previous' 			=> 'Précédent',
+    'Next' 				=> 'Suivant',
+    'Last' 				=> 'Dernier',
 
-        // Admin Edition
-        'Edit game' 		=> 'Edition du jeu',
-        'Welcome block' 	=> 'Bloc de bienvenue',
-        'Save' 				=> 'Sauvegarder',
+    // Admin Edition
+    'Edit game' 		=> 'Edition du jeu',
+    'Welcome block' 	=> 'Bloc de bienvenue',
+    'Save' 				=> 'Sauvegarder',
 
-        // Admin Game Form
-        'Game Type' 		=> 'type de jeu',
-        'Lottery' 			=> 'Tirage au sort',
-        'Lottery game' 		=> 'Créer un tirage au sort',
-        'Quiz game' 		=> 'Créer un quiz',
-        'Post & Vote game' 	=> 'Créer un Post & Vote',
-        'Main image' 		=> 'Image de couverture',
-        'Secondary image'	=> 'Image secondaire',
-        'Channel' 			=> 'Canal de diffusion',
-        'Start date' 		=> 'Date de début',
-        'End date' 			=> 'Date de fin',
-        'Game Mechanics' 	=> 'Mécanique du jeu',
-        'Homepage' 			=> 'Home page',
-        'General' 			=> 'Général',
+    // Admin Game Form
+    'Game Type' 		=> 'type de jeu',
+    'Lottery' 			=> 'Tirage au sort',
+    'Lottery game' 		=> 'Créer un tirage au sort',
+    'Quiz game' 		=> 'Créer un quiz',
+    'Post & Vote game' 	=> 'Créer un Post & Vote',
+    'Main image' 		=> 'Image de couverture',
+    'Secondary image'	=> 'Image secondaire',
+    'Channel' 			=> 'Canal de diffusion',
+    'Start date' 		=> 'Date de début',
+    'End date' 			=> 'Date de fin',
+    'Game Mechanics' 	=> 'Mécanique du jeu',
+    'Homepage' 			=> 'Home page',
+    'General' 			=> 'Général',
 
-        // Admin Leaderboard list
-        'User' 				=> 'Joueur',
-        'Points' 			=> 'Points',
-        'Register a new customer' => 'Inscrire un nouveau joueur',
-		
-		// Admin Quiz
-		'Create quiz' => 'Créer un quizz',
-		'Edit quiz' 	=> 'Editer un quizz',
-		
-        // Admin Quiz List
-        'Add new question' 	=> 'Ajouter une question',
+    // Admin Leaderboard list
+    'User' 				=> 'Joueur',
+    'Points' 			=> 'Points',
+    'Register a new customer' => 'Inscrire un nouveau joueur',
+	
+	// Admin Quiz
+	'Create quiz' => 'Créer un quizz',
+	'Edit quiz' 	=> 'Editer un quizz',
+	
+    // Admin Quiz List
+    'Add new question' 	=> 'Ajouter une question',
 
-        // Admin Quiz options questions
-        'Answers'			=> 'Réponse',
-        'List of answers' 	=> 'Liste des réponses',
-        'Single response' 	=> 'Réponse unique',
-        'Multiple choice' 	=> 'Choix multiple',
-        'Input field' 		=> 'Champ de saisie',
-		
-		// Admin Post & Vote
-		'Create Post & Vote' => 'Créer un Post & Vote',
-		'Edit Post & Vote' 	=> 'Editer un Post & Vote',
-		
-        // Admin Post & vote Options
-        'Edit' 				=> 'Editer',
-        'Generate Form' 	=> 'Générer le formulaire',
+    // Admin Quiz options questions
+    'Answers'			=> 'Réponse',
+    'List of answers' 	=> 'Liste des réponses',
+    'Single response' 	=> 'Réponse unique',
+    'Multiple choice' 	=> 'Choix multiple',
+    'Input field' 		=> 'Champ de saisie',
+	
+	// Admin Post & Vote
+	'Create Post & Vote' => 'Créer un Post & Vote',
+	'Edit Post & Vote' 	=> 'Editer un Post & Vote',
+	
+    // Admin Post & vote Options
+    'Edit' 				=> 'Editer',
+    'Generate Form' 	=> 'Générer le formulaire',
 
-        // Front game
-        'lottery' 			=> 'Tirage au sort',
-        'Quiz' 				=> 'Quizz',
-        'instantwin' 		=> 'Instant gagnant',
-        'quiz' 				=> 'Quizz',
-        'postvote' 			=> 'Post & Vote',
+    // Front game
+    'lottery' 			=> 'Tirage au sort',
+    'Quiz' 				=> 'Quizz',
+    'instantwin' 		=> 'Instant gagnant',
+    'quiz' 				=> 'Quizz',
+    'postvote' 			=> 'Post & Vote',
 
-        // Front game Quiz
-        'Previous questions' => 'Questions précédentes',
-        'Next questions' 	=> 'Questions suivantes',
+    // Front game Quiz
+    'Previous questions' => 'Questions précédentes',
+    'Next questions' 	=> 'Questions suivantes',
 
-        // Admin
-        'Admin' 			=> 'Administration',
-        'Dashboard' 		=> 'Tableau de bord',
-        'New Action' 		=> 'Nouvelle action',
-        'System' 			=> 'Système',
-        'Settings' 			=> 'Paramètres',
-        'Edit FAQ' 			=> 'Nouvelle FAQ',
-        'Add new page' 		=> 'Ajouter une nouvelle page',
-        'Create' 			=> 'Créer',
-        'title' 			=> 'Titre',
-        'identifier' 		=> 'Identifiant',
-        'is_active' 		=> 'Est activé',
-        'sort_order' 		=> 'Ordre de tri',
-        'Add new block' 	=> 'Ajouter un bloc de contenu',
-        'Block' 			=> 'Créer un bloc de contenu',
-        'Content' 			=> 'Contenu',
-        'Add' 				=> 'Ajouter',
-        'Dynarea' 			=> 'Zone cible',
-        'Dynablocks' 		=> 'Blocs dynamiques',
-        'Available Dynareas' => 'Zone cible disponible',
-		
-		// Admin Instantwin
-		'Create instant win' => 'Créer un instant gagnant',
-		'Edit instant win' 	=> 'Editer un instant gagnant',
-		
-        // Admin Instantwin occurence
-        'Add occurrence'	=> 'Ajouter un instant gagnant',
-        'Occurrence Date' 	=> 'Date de l\'instant gagnant',
-        'Edit occurrence'	=> 'Editer un instant gagnant',
-        
-		// Front post&vote email moderation
-		'This post has been reported in a game.' => 'Ce post a été signalé dans un jeu.',
-		
-        // Front post&vote
-        'This field contains more than %s characters' => 'Ce champ contient plus de %s caractères',
-		
-		// Admin Lottery
-		'Create lottery' => 'Créer un tirage au sort',
-		'Edit lottery' 	=> 'Editer un tirage au sort',
+    // Admin
+    'Admin' 			=> 'Administration',
+    'Dashboard' 		=> 'Tableau de bord',
+    'New Action' 		=> 'Nouvelle action',
+    'System' 			=> 'Système',
+    'Settings' 			=> 'Paramètres',
+    'Edit FAQ' 			=> 'Nouvelle FAQ',
+    'Add new page' 		=> 'Ajouter une nouvelle page',
+    'Create' 			=> 'Créer',
+    'title' 			=> 'Titre',
+    'identifier' 		=> 'Identifiant',
+    'is_active' 		=> 'Est activé',
+    'sort_order' 		=> 'Ordre de tri',
+    'Add new block' 	=> 'Ajouter un bloc de contenu',
+    'Block' 			=> 'Créer un bloc de contenu',
+    'Content' 			=> 'Contenu',
+    'Add' 				=> 'Ajouter',
+    'Dynarea' 			=> 'Zone cible',
+    'Dynablocks' 		=> 'Blocs dynamiques',
+    'Available Dynareas' => 'Zone cible disponible',
+	
+	// Admin Instantwin
+	'Create instant win' => 'Créer un instant gagnant',
+	'Edit instant win' 	=> 'Editer un instant gagnant',
+	
+    // Admin Instantwin occurence
+    'Add occurrence'	=> 'Ajouter un instant gagnant',
+    'Occurrence Date' 	=> 'Date de l\'instant gagnant',
+    'Edit occurrence'	=> 'Editer un instant gagnant',
+    
+	// Front post&vote email moderation
+	'This post has been reported in a game.' => 'Ce post a été signalé dans un jeu.',
+	
+    // Front post&vote
+    'This field contains more than %s characters' => 'Ce champ contient plus de %s caractères',
+	
+	// Admin Lottery
+	'Create lottery' => 'Créer un tirage au sort',
+	'Edit lottery' 	=> 'Editer un tirage au sort',
+
+    // State
+    'launching' => '<span class="label label-warning">Lancement</span>',
+    'in progress' => '<span class="label label-success">En cours</span>',
+    'finished' => '<span class="label label-inverse">Terminé</span>',
 
 );
