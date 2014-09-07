719a330985060a3891cd7ca85102af3f45cf5dd7
GrG <gregory.besson@adfab.fr>
2014-09-07 17:14:21 +0200

    Theme selection fix

--- src/PlaygroundGame/Module.php
+++ src/PlaygroundGame/Module.php
@@ -36,7 +36,11 @@ class Module
 
         $config = $e->getConfigListener()->getMergedConfig(false);
         
-        $parentTheme = array($config['design']['frontend']['package'], $config['design']['frontend']['theme']);
+        if(isset($config['design']) && isset($config['design']['frontend'])){
+            $parentTheme = array($config['design']['frontend']['package'], $config['design']['frontend']['theme']);
+        } else {
+            $parentTheme = array('playground','base');
+        }
     
         // If custom games need a specific route. I create these routes
         if(isset($config['custom_games'])){
