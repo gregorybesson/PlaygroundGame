4fbbac668e88ca3d9d7ea32acaaafc912b7d782f
GrG <gregory.besson@adfab.fr>
2014-09-10 01:49:11 +0200

    PLAYGROUND-476

--- src/PlaygroundGame/Service/Quiz.php
+++ src/PlaygroundGame/Service/Quiz.php
@@ -123,7 +123,7 @@ class Quiz extends Game implements ServiceManagerAwareInterface
             ErrorHandler::stop(true);
         }
 
-        if($data['delete_image'] && empty($data['upload_image']['tmp_name'])) {
+        if(isset($data['delete_image']) && empty($data['upload_image']['tmp_name'])) {
             ErrorHandler::start();
             $image = $question->getImage();
             $image = str_replace($media_url, '', $image);
