6d8946313d0a5476678fecd609bb1045eed5104b
Greg Besson <bessong@gmail.com>
2019-04-07 18:27:48 +0200

    translation files order

--- config/module.config.php
+++ config/module.config.php
@@ -130,16 +130,16 @@ return array(
         'translation_file_patterns' => array(
             array(
                 'type' => 'phpArray',
-                'base_dir' => __DIR__ . '/../../../../language',
+                'base_dir' => __DIR__ . '/../language',
                 'pattern' => '%s.php',
                 'text_domain' => 'playgroundgame'
             ),
             array(
                 'type' => 'phpArray',
-                'base_dir' => __DIR__ . '/../language',
+                'base_dir' => __DIR__ . '/../../../../language',
                 'pattern' => '%s.php',
                 'text_domain' => 'playgroundgame'
-            )
+            ),
         )
     ),
 
