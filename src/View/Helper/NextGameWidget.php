1f6227935fc78303952f3ebdcf378305526d380d
Greg Besson <bessong@gmail.com>
2019-04-13 19:37:17 -0700

    new widget for games to come

---
+++ src/View/Helper/NextGameWidget.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace PlaygroundGame\View\Helper;
+
+use Zend\View\Helper\AbstractHelper;
+use Interop\Container\ContainerInterface;
+
+class NextGameWidget extends AbstractHelper
+{
+    /**
+     * @var GameService
+     */
+    protected $gameService;
+
+    public function __construct(\PlaygroundGame\Service\Game $gameService) 
+    {
+        return $this->gameService = $gameService;
+    }
+
+    /**
+     * __invoke
+     *
+     * @access public
+     * @param  array  $options array of options
+     * @return string
+     */
+    public function __invoke($dateStart = null, $dateEnd = null, $classType = null, $cost = null, $order = null, $dir = 'DESC')
+    {
+        $games = $this->getGameService()->getNextGames($dateStart, $dateEnd, $classType, $cost, $order, $dir);
+
+        return $games;
+    }
+
+    /**
+     * Get gameService.
+     *
+     * @return GameService
+     */
+    public function getGameService()
+    {
+        return $this->gameService;
+    }
+}
